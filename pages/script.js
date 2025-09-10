cf900b7942290ee281566820f966add67b76d223
shulamitmorjossef <shulamitmorjossef@gmail.com>
2025-09-10 13:43:00 +0300

    Added baic files

--- pages/script.js
+++ pages/script.js
@@ -1,12 +1,10 @@
 let slideIndex = 0;
 showSlides(slideIndex);
 
-// מעבר ידני
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// פונקציה להצגת השקופיות
 function showSlides(n) {
   let slides = document.getElementsByClassName("slide");
   if (n >= slides.length) { slideIndex = 0; }
@@ -17,11 +15,9 @@ function showSlides(n) {
   slides[slideIndex].style.display = "block";
 }
 
-// מאזינים לכפתורים
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".prev").addEventListener("click", () => plusSlides(-1));
   document.querySelector(".next").addEventListener("click", () => plusSlides(1));
 
-  // מעבר אוטומטי כל 5 שניות
   setInterval(() => plusSlides(1), 5000);
 });
