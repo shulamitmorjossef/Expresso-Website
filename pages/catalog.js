6f3c343c9d595fb1549960bf2094bce44e298b72
shulamitmorjossef <shulamitmorjossef@gmail.com>
2025-09-10 16:07:42 +0300

    fixes all pages

--- pages/catalog.js
+++ pages/catalog.js
@@ -19,3 +19,25 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const images = document.querySelectorAll(".lightbox-img");
+const closeBtn = document.querySelector(".close");
+
+images.forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "flex";  // flex כדי למרכז את התמונה
+    lightboxImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+// לסגירה גם אם לוחצים על הרקע השחור
+lightbox.addEventListener("click", (e) => {
+  if(e.target === lightbox) lightbox.style.display = "none";
+});
+
